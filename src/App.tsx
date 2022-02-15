3663fa914fbae26843586d2c6d7ebc78884e95cd
joshamore <joshamore31@gmail.com>
2022-02-15 16:47:47 +1100

    Init with CRA

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Init</p>
+      </header>
+
+      <div>
+        <p>Init</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
